7c3c01a8e605b46edb23821642ba4f0133b19ac1
Maycon Pires <maycon.pires@inpe.br>
2026-02-05 17:02:41 -0300

    add: fp

---
+++ shortener-api/src/urls/dto/create-url/create-url.spec.ts
@@ -0,0 +1,7 @@
+import { CreateUrl } from './create-url';
+
+describe('CreateUrl', () => {
+  it('should be defined', () => {
+    expect(new CreateUrl()).toBeDefined();
+  });
+});
---
+++ shortener-api/src/urls/dto/update-url/update-url.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateUrl } from './update-url';
+
+describe('UpdateUrl', () => {
+  it('should be defined', () => {
+    expect(new UpdateUrl()).toBeDefined();
+  });
+});
