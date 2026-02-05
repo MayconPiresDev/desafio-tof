14a56165f7c6b496b3313982461248852aec3cb2
Maycon Pires <maycon.pires@inpe.br>
2026-02-05 17:21:55 -0300

    feat: complete project scaffold with modules structure

--- shortener-api/src/global/global-pipe/global-pipe.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GlobalPipePipe } from './global-pipe.pipe';
-
-describe('GlobalPipePipe', () => {
-  it('should be defined', () => {
-    expect(new GlobalPipePipe()).toBeDefined();
-  });
-});
--- shortener-api/src/global/global-pipe/global-pipe.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class GlobalPipePipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
-  }
-}
